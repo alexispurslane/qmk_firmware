85caf4f06f99f98d2d3eb61fa9d7d807aa218b14
Alexis Purslane <alexispurslane@pm.me>
2024-08-30 09:25:32 -0400

    Make left spacebar into minus/underscore key

--- keyboards/xbows/knight_plus/keymaps/alexispurslane/keymap.c
+++ keyboards/xbows/knight_plus/keymaps/alexispurslane/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,  KC_E,   KC_R,   KC_T,    KC_Y,    KC_U,  KC_I,   KC_O,    KC_P,   KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
 		KC_CAPS, KC_A,    KC_S,  KC_D,   KC_F,   KC_G,    KC_BSPC, KC_H,  KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,  KC_PGDN,
 		KC_LSFT, KC_Z,    KC_X,  KC_C,   KC_V,   KC_B,    KC_ENT,  KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP,
-		KC_LCTL, KC_LALT, KC_LGUI,       KC_SPC, KC_LCTL, KC_LSFT, KC_SPC,        KC_RGUI, MO(2),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+		KC_LCTL, KC_LALT, KC_LGUI,       KC_MINS, KC_LCTL, KC_LSFT, KC_SPC,        KC_RGUI, MO(2),  KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
   [1] = LAYOUT(
 	    KC_ESC,  DF(0),   DF(1), TG(2),  KC_F4,  KC_F5,   KC_F6,   KC_F7, KC_F8,  KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_DEL,  KC_PSCR,
@@ -51,6 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT(
     QK_BOOT,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_CALC,   KC_MYCM,  KC_MSEL,   KC_MAIL,   NK_TOGG,   EE_CLR,
+
     KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,             KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_NUM,
     RGB_TOG,   RGB_MOD,  RGB_VAI,  RGB_HUI,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_HOME,
     KC_TRNS,   RGB_SPD,  RGB_VAD,  RGB_SPI,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_END,
@@ -75,6 +76,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case KC_0:
     case KC_LBRC:
     case KC_RBRC:
+    case KC_BSLS:
         {
             if (record->event.pressed) {
                 if (mod_state & MOD_MASK_SHIFT) {
